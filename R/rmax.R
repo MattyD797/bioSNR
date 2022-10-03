e4a43c0d2d76797b92594b9318ea34c5d41d9b36
leabouffaut <lea.bouffaut@hotmail.fr>
2022-10-03 15:46:15 -0400

    Add documentation

--- R/rmax.R
+++ R/rmax.R
@@ -1,3 +1,6 @@
+#' This function solves the sonar equation for the maximum range, for cylindrical spreading and no absorption
+#' e.g seek the range at wich the TL =  SL - NL - DT;
+
 rmax <- function(TL, boolR=T){
   r <- 10^(TL/10)/1000
   if(boolR){
